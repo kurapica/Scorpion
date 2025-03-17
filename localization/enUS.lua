0476635b36391fafa6b0cf72ddeab9e17b70f932
kurapica <kurapica125@outlook.com>
2025-03-18 00:21:50 +0800

    init

---
+++ localization/enUS.lua
@@ -0,0 +1,2 @@
+local L                         = Scorpio("Scorpion")._Locale("enUS", true)
+if not L then return end
---
+++ localization/zhCN.lua
@@ -0,0 +1,2 @@
+local L                         = Scorpio("Scorpion")._Locale("zhCN")
+if not L then return end
